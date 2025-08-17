6f997197d3513fc8ad31e51a3ebf5867653a33b4
Kadava <kadavamail@gmail.com>
2025-08-17 17:53:59 +0100

    updated customaudiohub

--- KadVault/PluginMain.cs
+++ KadVault/PluginMain.cs
@@ -24,8 +24,8 @@ public class PluginMain : Plugin<Config>
         Schematic.SchematicSpawned += SchematicHandler.Spawned;
         Schematic.ButtonInteracted += SchematicHandler.ButtonInteracted;
         CustomHandlersManager.RegisterEventsHandler(labApiHandler);
-        AudioClipStorage.LoadClip(CustomAudioHub.Main.Instance.MakeFilePath(Instance.Config.SafeOpeningSFX), "DoorOpenSFX");
-        AudioClipStorage.LoadClip(CustomAudioHub.Main.Instance.MakeFilePath(Instance.Config.AlarmSFX), "AlarmSFX");
+        AudioClipStorage.LoadClip(CustomAudioHub.Main.MakeFilePath(Instance.Config.SafeOpeningSFX), "DoorOpenSFX");
+        AudioClipStorage.LoadClip(CustomAudioHub.Main.MakeFilePath(Instance.Config.AlarmSFX), "AlarmSFX");
     }
 
     public override void Disable()
