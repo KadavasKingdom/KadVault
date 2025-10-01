db4a9dc5629c07f553d8644535cdee30eb00d6bc
Kadava <kadavamail@gmail.com>
2025-10-01 01:59:08 +0100

    Removes vault access card when used

--- KadVault/Handlers/SchematicHandler.cs
+++ KadVault/Handlers/SchematicHandler.cs
@@ -61,6 +61,7 @@ internal class SchematicHandler
                     continue;
 
                 OpenVault(ev);
+                item.DropItem().Destroy();
                 return;
             }
 
