9bbc414ef08aff5bbfcd157dc898e4213a71c9c5
Fallout001 <fallingout081@gmail.com>
2025-10-03 16:58:22 -0400

    fix text

--- KadVault/Handlers/SchematicHandler.cs
+++ KadVault/Handlers/SchematicHandler.cs
@@ -81,7 +81,7 @@ internal class SchematicHandler
             RoundStatsTracker.AddStatEvent("KadVault", "Vault", "Vault Button Engaged", $" Player = {ev.Player.Nickname} , Class = {ev.Player.Role}");
         }
         float xptogive = 100f;
-        XPSystem.BackEnd.XpSystemAPI.AddXP(ev.Player, xptogive, $"Open Vault [+{xptogive}]");
+        XPSystem.BackEnd.XpSystemAPI.AddXP(ev.Player, xptogive, $"Open <color=red>Vault</color> [+{xptogive}]");
         Cassie.Message("ALERT . . LIGHT CONTAINMENT ZONE OMEGA ARMORY ACCESS AUTHORIZED . . OPENING SEQUENCE HAS BEGUN . . .", false, true, true);
         for (int i = 0; i < PluginMain.Instance.Vault.SafeDoorAnim.Animators.Count; i++)
         {
